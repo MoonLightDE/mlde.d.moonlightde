df0ac1ece6de3f5c42537997864efba5dbce1b9c
Alexis López Zubieta <azubieta@estudiantes.uci.cu>
2014-03-25 21:47:26 -0400

    SettingsProfile class implemented.

---
+++ src/core/c++/SettingsProfile.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2014 Moonlight Desktop Environment Team
+ * Authors:
+ * Alexis López Zubieta
+ * This file is part of Moonlight Desktop Environment.
+ *
+ * Moonlight Desktop Environment is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Moonlight Desktop Environment is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Moonlight Desktop Environment. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SETTINGSPROFILE_H
+#define	SETTINGSPROFILE_H
+
+#include "core/ICore.h"
+
+#include <QSettings>
+#include <QString>
+
+class SettingsProfile : public CoreContext::ISettingsProfile {
+public:
+    SettingsProfile(QString profileName);
+
+    QSettings * getSettingsOf (QObject * object = 0);
+    QSettings * getSettingsOf (const QString & objectPath);
+
+    virtual ~SettingsProfile();
+private:
+    QString profileName;
+};
+
+#endif	/* SETTINGSPROFILE_H */
+
