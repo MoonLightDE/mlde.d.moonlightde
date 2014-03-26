9d7b367c63f1c2f5a0ccd3b0ad9c9cb4019272ce
Alexis López Zubieta <azubieta@estudiantes.uci.cu>
2014-03-26 09:44:34 -0400

    Adding class "Core".

---
+++ src/core/c++/Core.cpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2014 Moonlight Desktop Environment Team
+ * Authors:
+ * Alexis López Zubieta
+ * This file is part of Moonlight Desktop Environment.
+ *
+ * Moonlight Desktop Environment is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Moonlight Desktop Environment is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Moonlight Desktop Environment. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "Core.h"
+#include "SettingsProfile.h"
+#include "ModuleManager.h"
+#include "Environment.h"
+
+#include <usGetModuleContext.h>
+#include <usModuleContext.h>
+#include <usServiceProperties.h>
+
+#include <QDebug>
+
+using namespace us;
+
+Core::Core(QString profile, QString aditionalLibsPath) {
+    qDebug() << "Constructing core.";
+    ModuleContext * context  = GetModuleContext();
+
+    // Core services creation
+    settingsProfile = new SettingsProfile(profile);
+    {
+        ServiceProperties props;
+        context->RegisterService<CoreContext::ISettingsProfile>(settingsProfile, props);
+    }
+    moduleManager = new ModuleManager(aditionalLibsPath);
+    {
+        ServiceProperties props;
+        context->RegisterService<CoreContext::IModuleManager>(moduleManager, props);
+    }
+
+    environment = new Environment();
+    {
+        ServiceProperties props;
+        context->RegisterService<CoreContext::IEnvironment>(environment, props);
+    }
+
+    {
+        ServiceProperties props;
+        context->RegisterService<CoreContext::ICore>(this, props);
+    }
+}
+
+void Core::start() {
+    qDebug() << "Starting core.";
+    // Loading modules
+    QSettings * profile = settingsProfile->getSettingsOf();
+    moduleManager->loadFromProfile(profile);
+    qDebug() << "Aviable modules: " << moduleManager->listAviableModules();
+    qDebug() << "Active modules: " << moduleManager->listActiveModules();
+
+    emit(started());
+}
+
+void Core::finish() {
+    qDebug() << "Destroing core.";
+}
+
+Core::~Core() {
+}
+
