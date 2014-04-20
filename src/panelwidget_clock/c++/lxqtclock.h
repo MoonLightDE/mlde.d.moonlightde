8ff1827c2abd1ede1b2dc86159af4b3d6d9fe4dc
Alexis López Zubieta <azubieta@estudiantes.uci.cu>
2014-04-20 08:05:23 -0400

    Porting LxqtPanel Clock.

---
+++ src/panelwidget_clock/c++/lxqtclock.h
@@ -0,0 +1,102 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * LXDE-Qt - a lightweight, Qt based, desktop toolset
+ * http://razor-qt.org
+ *
+ * Copyright: 2010-2013 Razor team
+ * Authors:
+ *   Christopher "VdoP" Regali
+ *   Alexander Sokoloff <sokoloff.a@gmail.com>
+ *   Maciej Płaza <plaza.maciej@gmail.com>
+ *   Kuzma Shapran <kuzma.shapran@gmail.com>
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
+#ifndef LXQTCLOCK_H
+#define LXQTCLOCK_H
+
+
+#include "lxqtclockconfiguration.h"
+
+#include <lxqt/lxqtrotatedwidget.h>
+
+#include <QtCore/QString>
+#include <QSettings>
+
+class QLabel;
+class QDialog;
+class QTimer;
+
+class LxQtClock : public QObject {
+    Q_OBJECT
+public:
+    LxQtClock();
+    ~LxQtClock();
+
+    QString themeId() const {
+        return "Clock";
+    }
+
+    QWidget *widget() {
+        return mMainWidget;
+    }
+    QDialog *configureDialog();
+    void settingsChanged();
+
+    void activated();
+
+    bool isSeparate() const {
+        return true;
+    }
+
+    void realign();
+
+public slots:
+    void updateTime();
+
+protected:
+    bool eventFilter(QObject *watched, QEvent *event);
+
+private:
+    QTimer* mClockTimer;
+    QWidget *mMainWidget;
+    QWidget *mContent;
+    LxQt::RotatedWidget* mRotatedWidget;
+    QLabel* mTimeLabel;
+    QLabel* mDateLabel;
+    QString mClockFormat;
+    QString mToolTipFormat;
+    QDialog* mCalendarDialog;
+    QString mTimeFormat;
+    QString mDateFormat;
+    bool mDateOnNewLine;
+    bool mUseUTC;
+    Qt::DayOfWeek mFirstDayOfWeek;
+    bool mAutoRotate;
+
+    QDateTime currentDateTime();
+    void showTime(const QDateTime &);
+    void restartTimer(const QDateTime&);
+
+    QSettings *m_settings;
+private slots:
+    void updateMinWidth();
+};
+
+#endif
