887705e31cb588bb7d7d31852735fda337a687bd
Alexis López Zubieta <azubieta@estudiantes.uci.cu>
2014-05-20 11:47:54 -0400

    Add pane-quicklaunchers.

---
+++ src/panel-quicklauchers/c++/quicklaunchaction.cpp
@@ -0,0 +1,131 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * LXDE-Qt - a lightweight, Qt based, desktop toolset
+ * http://razor-qt.org
+ *
+ * Copyright: 2010-2011 Razor team
+ * Authors:
+ *   Petr Vanek <petr@scribus.info>
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
+
+#include "quicklaunchaction.h"
+
+#include <QtDebug>
+#include <QProcess>
+#include <QUrl>
+#include <QMessageBox>
+#include <QDesktopServices>
+#include <QFileIconProvider>
+
+#include <qt5xdg/xdgdesktopfile.h>
+#include <qt5xdg/xdgicon.h>
+#include <qt5xdg/xdgmime.h>
+
+
+QuickLaunchAction::QuickLaunchAction(const QString & name,
+                                     const QString & exec,
+                                     const QString & icon,
+                                     QWidget * parent)
+    : QAction(name, parent),
+      m_valid(true)
+{
+    m_type = ActionLegacy;
+    
+    m_settingsMap["name"] = name;
+    m_settingsMap["exec"] = exec;
+    m_settingsMap["icon"] = icon;
+
+    if (icon.isNull())
+        setIcon(XdgIcon::defaultApplicationIcon());
+    else
+        setIcon(QIcon(icon));
+
+    setData(exec);
+    connect(this, SIGNAL(triggered()), this, SLOT(execAction()));
+}
+
+QuickLaunchAction::QuickLaunchAction(const XdgDesktopFile * xdg,
+                                     QWidget * parent)
+    : QAction(parent),
+      m_valid(true)
+{
+    m_type = ActionXdg;
+    
+    m_settingsMap["desktop"] = xdg->fileName();
+
+    QString title(xdg->localizedValue("Name").toString());
+    QString gn(xdg->localizedValue("GenericName").toString());
+    if (!gn.isEmpty())
+        title += " (" + gn + ")";
+    setText(title);
+
+    setIcon(xdg->icon(XdgIcon::defaultApplicationIcon()));
+
+    setData(xdg->fileName());
+    connect(this, SIGNAL(triggered()), this, SLOT(execAction()));
+}
+
+QuickLaunchAction::QuickLaunchAction(const QString & fileName, QWidget * parent)
+    : QAction(parent),
+      m_valid(true)
+{
+    m_type = ActionFile;
+    setText(fileName);
+    setData(fileName);
+    
+    m_settingsMap["file"] = fileName;
+
+    QFileInfo fi(fileName);
+    if (fi.isDir())
+    {
+        QFileIconProvider ip;
+        setIcon(ip.icon(fi));
+    }
+    else
+    {
+        XdgMimeInfo mi(fi);
+        setIcon(mi.icon());
+    }
+    
+    connect(this, SIGNAL(triggered()), this, SLOT(execAction()));
+}
+
+void QuickLaunchAction::execAction()
+{
+    QString exec(data().toString());
+    qDebug() << "execAction" << exec;
+    switch (m_type)
+    {
+        case ActionLegacy:
+            QProcess::startDetached(exec);
+            break;
+        case ActionXdg:
+        {
+            XdgDesktopFile * xdg = XdgDesktopFileCache::getFile(exec);
+            if (xdg->isValid())
+                xdg->startDetached();
+            break;
+        }
+        case ActionFile:
+            QDesktopServices::openUrl(QUrl(exec));
+            break;
+    }
+}
---
+++ src/panel-quicklauchers/c++/quicklaunchbutton.cpp
@@ -0,0 +1,171 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * LXDE-Qt - a lightweight, Qt based, desktop toolset
+ * http://razor-qt.org
+ *
+ * Copyright: 2010-2011 Razor team
+ * Authors:
+ *   Petr Vanek <petr@scribus.info>
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
+#include <QAction>
+#include <QMenu>
+#include <QStylePainter>
+#include <QStyleOptionToolButton>
+#include <QMouseEvent>
+#include <QDrag>
+#include <QApplication>
+#include <qt5xdg/xdgicon.h>
+
+#include "quicklaunchbutton.h"
+#include "lxqtquicklaunch.h"
+
+#define MIMETYPE "x-lxqt/quicklaunch-button"
+
+
+QuickLaunchButton::QuickLaunchButton(QuickLaunchAction * act, QWidget * parent)
+    : QToolButton(parent),
+      mAct(act)
+{
+    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    setAcceptDrops(true);
+
+    setDefaultAction(mAct);
+    mAct->setParent(this);
+
+    mMoveLeftAct = new QAction(XdgIcon::fromTheme("go-previous"), tr("Move left"), this);
+    connect(mMoveLeftAct, SIGNAL(triggered()), this, SIGNAL(movedLeft()));
+
+    mMoveRightAct = new QAction(XdgIcon::fromTheme("go-next"), tr("Move right"), this);
+    connect(mMoveRightAct, SIGNAL(triggered()), this, SIGNAL(movedRight()));
+
+
+    mDeleteAct = new QAction(XdgIcon::fromTheme("dialog-close"), tr("Remove from quicklaunch"), this);
+    connect(mDeleteAct, SIGNAL(triggered()), this, SLOT(selfRemove()));
+    addAction(mDeleteAct);
+    mMenu = new QMenu(this);
+    mMenu->addAction(mAct);
+    mMenu->addSeparator();
+    mMenu->addAction(mMoveLeftAct);
+    mMenu->addAction(mMoveRightAct);
+    mMenu->addSeparator();
+    mMenu->addAction(mDeleteAct);
+
+
+    setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(this, SIGNAL(customContextMenuRequested(const QPoint&)),
+            this, SLOT(this_customContextMenuRequested(const QPoint&)));
+}
+
+QuickLaunchButton::~QuickLaunchButton()
+{
+    //m_act->deleteLater();
+}
+
+QHash<QString,QString> QuickLaunchButton::settingsMap()
+{
+    Q_ASSERT(mAct);
+    return mAct->settingsMap();
+}
+
+
+void QuickLaunchButton::this_customContextMenuRequested(const QPoint & pos)
+{
+    LxQtQuickLaunch *panel = qobject_cast<LxQtQuickLaunch*>(parent());
+
+    mMoveLeftAct->setEnabled( panel && panel->indexOfButton(this) > 0);
+    mMoveRightAct->setEnabled(panel && panel->indexOfButton(this) < panel->countOfButtons() - 1);
+    mMenu->popup(mapToGlobal(pos));
+}
+
+void QuickLaunchButton::selfRemove()
+{
+    emit buttonDeleted();
+}
+
+void QuickLaunchButton::paintEvent(QPaintEvent *)
+{
+    // Do not paint that ugly "has menu" arrow
+    QStylePainter p(this);
+    QStyleOptionToolButton opt;
+    initStyleOption(&opt);
+    opt.features &= (~ QStyleOptionToolButton::HasMenu);
+    p.drawComplexControl(QStyle::CC_ToolButton, opt);
+}
+
+void QuickLaunchButton::mousePressEvent(QMouseEvent *e)
+{
+    if (e->button() == Qt::LeftButton && e->modifiers() == Qt::ControlModifier)
+    {
+        mDragStart = e->pos();
+        return;
+    }
+
+    QToolButton::mousePressEvent(e);
+}
+
+void QuickLaunchButton::mouseMoveEvent(QMouseEvent *e)
+{
+    if (!(e->buttons() & Qt::LeftButton))
+    {
+        return;
+    }
+
+    if ((e->pos() - mDragStart).manhattanLength() < QApplication::startDragDistance())
+    {
+        return;
+    }
+
+    if (e->modifiers() != Qt::ControlModifier)
+    {
+        return;
+    }
+
+    QDrag *drag = new QDrag(this);
+    ButtonMimeData *mimeData = new ButtonMimeData();
+    mimeData->setButton(this);
+    drag->setMimeData(mimeData);
+
+    drag->exec(Qt::MoveAction);
+
+    // Icon was droped outside the panel, remove button
+    if (!drag->target())
+    {
+        selfRemove();
+    }
+}
+
+void QuickLaunchButton::dragMoveEvent(QDragMoveEvent * e)
+{
+    if (e->mimeData()->hasFormat(MIMETYPE))
+        e->acceptProposedAction();
+    else
+        e->ignore();
+}
+
+void QuickLaunchButton::dragEnterEvent(QDragEnterEvent *e)
+{
+    const ButtonMimeData *mimeData = qobject_cast<const ButtonMimeData*>(e->mimeData());
+    if (mimeData && mimeData->button())
+    {
+        emit switchButtons(mimeData->button(), this);
+    }
+}
+
