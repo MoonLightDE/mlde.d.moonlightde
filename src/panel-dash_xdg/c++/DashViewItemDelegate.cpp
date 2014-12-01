39082a8a5613465ec6eac0f08a9d2a59e1faa817
akiel <cogitoergosum>
2014-12-01 15:59:05 -0500

    Agregada licencia a nuevos archivos

--- src/panel-dash_xdg/c++/DashViewItemDelegate.cpp
+++ src/panel-dash_xdg/c++/DashViewItemDelegate.cpp
@@ -1,10 +1,22 @@
-/* 
- * File:   DashViewItemDelegate.cpp
- * Author: akiel
- * 
- * Created on November 18, 2014, 12:54 PM
+/*
+ * Copyright (C) 2014 Moonlight Desktop Environment Team
+ * Authors:
+ *      Jorge Alberto Díaz Orozco
+ * This file is part of Moonlight Desktop Environment.
+ *
+ * Moonlight Desktop Environment is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Moonlight Desktop Environment is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Moonlight Desktop Environment. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include "DashViewItemDelegate.h"
 
 #include <QApplication>
