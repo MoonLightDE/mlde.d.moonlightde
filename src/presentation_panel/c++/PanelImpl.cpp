dc76029d39b4c0ad7376a06d839a5985e818fc0e
Ruben Salvador Garcia San Juan <rssanjuan@uci.cu>
2015-02-24 15:19:43 -0500

    Fixed borders relationship with the widgets

--- src/presentation_panel/c++/PanelImpl.cpp
+++ src/presentation_panel/c++/PanelImpl.cpp
@@ -170,11 +170,8 @@ void PanelImpl::removeWidgetFactory(presentation_panel::WidgetFactory* widgetFac
 
 void PanelImpl::updateLayout() {
     // Destroy layout;
-    qDebug() << "Deleting old layout:";
     delete layout();
-    qDebug() << "Size of map:";
-    qDebug() << m_Widgets.size();
-    
+
     QHBoxLayout * newLayout = new QHBoxLayout(this);
 
     QMapIterator<QString, QPointer<QWidget> > i(m_Widgets);
